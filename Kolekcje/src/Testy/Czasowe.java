8db48b61661c448e78674336fb89f6abb4ed47cf
Avanrethus <kazuhi13@gmail.com>
2016-12-29 16:16:37 +0100

    Dodałem testy czasu

---
+++ Kolekcje/src/Testy/Czasowe.java
@@ -0,0 +1,16 @@
+
+package Testy;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+
+public class Czasowe {
+    public static void main(String [] args){
+        ArrayList<Integer> al = new ArrayList();
+        LinkedList<Integer> ll = new LinkedList();
+        long czasPrzed;
+        
+        czasPrzed = System.currentTimeMillis();
+    }
+}
